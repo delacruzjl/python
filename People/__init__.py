8e074b7263427d72c75dfbbf4a83e1ec0d4faebe
Jose DelaCruz <jodelac@microsoft.com>
2020-08-19 00:16:18 -0400

    passes a dictionary to match aggregation

--- People/__init__.py
+++ People/__init__.py
@@ -10,7 +10,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     connection_string = f"mongodb+srv://{os.environ['user']}:{os.environ['passwd']}@{os.environ['uri']}"
-    people = get_people_from_mongo_by_age(connection_string, 20)
+    people = get_people_from_mongo(connection_string, age = {'$gte': 40 }, name = {'$eq': 'Jesse Jordan'})
     result = parse_mongodb_results(people)
     return func.HttpResponse(result, mimetype='application/json')
 
@@ -22,16 +22,18 @@ def get_person_collection(client: mongo.MongoClient) -> mongo.collection:
     db = client.m121
     return db['people']
 
-def get_people_from_mongo_by_age(connection_string: str, age: int):
+def get_people_from_mongo(connection_string: str, **kwargs):
     client = connect_to_mongodb(connection_string)
     coll = get_person_collection(client)
     pipeline = [
-        {'$match': {'age': {'$gte': age }}},
+        {'$match': kwargs},
         {'$project': {'_id': 0, 'name': 1, 'age': 1}} 
     ]
 
     return coll.aggregate(pipeline)
 
 def parse_mongodb_results(people: mongo.CursorType):
+    # if not isinstance(people, mongo.CursorType):
+    #     raise TypeError(f'Cannot serialize invalid results.  Expected mongodb cursor, got {type(people)}')
     result = tuple(people)
     return json.dumps(result)
