7e746bbb91adbc8e61e130f3a4e524d8146e23ca
Jose DelaCruz <jodelac@microsoft.com>
2020-08-18 23:58:25 -0400

    uses the aggregation framework

--- People/__init__.py
+++ People/__init__.py
@@ -6,24 +6,6 @@ import pymongo as mongo
 import os
 import azure.functions as func
 
-def get_people_from_mongo_by_age(connection_string: str, age: int):
-    client = connect_to_mongodb(connection_string)
-    coll = get_person_collection(client)
-    return coll.find({'age': {'$gte': age }}, {'_id': 0, 'name': 1, 'age': 1})
-
-def connect_to_mongodb(connection_string: str) -> mongo.MongoClient:
-    logging.debug('connecting to mongodb: {}'.format(connection_string))
-    return mongo.MongoClient(connection_string)
-
-
-def get_person_collection(client: mongo.MongoClient) -> mongo.collection:
-    db = client.m121
-    return db['people']
-
-def parse_mongodb_results(people: mongo.CursorType):
-    result = tuple(people)
-    return json.dumps(result)
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
@@ -31,3 +13,25 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     people = get_people_from_mongo_by_age(connection_string, 20)
     result = parse_mongodb_results(people)
     return func.HttpResponse(result, mimetype='application/json')
+
+def connect_to_mongodb(connection_string: str) -> mongo.MongoClient:
+    logging.debug('connecting to mongodb: {}'.format(connection_string))
+    return mongo.MongoClient(connection_string)
+
+def get_person_collection(client: mongo.MongoClient) -> mongo.collection:
+    db = client.m121
+    return db['people']
+
+def get_people_from_mongo_by_age(connection_string: str, age: int):
+    client = connect_to_mongodb(connection_string)
+    coll = get_person_collection(client)
+    pipeline = [
+        {'$match': {'age': {'$gte': age }}},
+        {'$project': {'_id': 0, 'name': 1, 'age': 1}} 
+    ]
+
+    return coll.aggregate(pipeline)
+
+def parse_mongodb_results(people: mongo.CursorType):
+    result = tuple(people)
+    return json.dumps(result)
